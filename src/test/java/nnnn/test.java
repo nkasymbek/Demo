46856afd9ff54b1fcd2b6b2efa844a74034b5b5b
zhibek <zhibek926@gmail.com>
2018-04-21 18:36:18 -0500

    added main method

--- src/test/java/nnnn/test.java
+++ src/test/java/nnnn/test.java
@@ -1,10 +1,10 @@
 package nnnn;
 
 public class test {
-	
 	public static void main(String[] args) {
 		
 		System.out.println("Branch: Zhibek!!!");
+		
 		System.out.println("Collaboration!!!");
 		
 	}
